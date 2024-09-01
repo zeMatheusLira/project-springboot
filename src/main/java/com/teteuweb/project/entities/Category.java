0489dfa03b44eb5b6c1e51f0e0a2f3cc16299332
Zé Matheus <josemw86@gmail.com>
2024-08-31 23:29:00 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/teteuweb/project/entities/Category.java
+++ src/main/java/com/teteuweb/project/entities/Category.java
@@ -1,5 +1,6 @@
 package com.teteuweb.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,7 +17,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
--- src/main/java/com/teteuweb/project/entities/Product.java
+++ src/main/java/com/teteuweb/project/entities/Product.java
@@ -7,86 +7,89 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-@Entity
-@Table(name = "tb_product")
-public class Product implements Serializable {
+    @Entity
+    @Table(name = "tb_product")
+    public class Product implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String description;
-    private Double price;
-    private String imgUrl;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+        private String description;
+        private Double price;
+        private String imgUrl;
 
-    @Transient
-    private Set<Category> categories = new HashSet<>();// set garante que o produto nao se repita a instancia garante que ela comece vazia e não nula
+        @ManyToMany
+        @JoinTable(name = "tb_product_category",
+                joinColumns = @JoinColumn(name = "product_id"),
+                inverseJoinColumns = @JoinColumn(name = "category_id"))
+        private Set<Category> categories = new HashSet<>();// set garante que o produto nao se repita a instancia garante que ela comece vazia e não nula
 
-    public Product() {
+        public Product() {
+        }
+
+        public Product(Long id, String name, String description, Double price, String imgUrl) {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.imgUrl = imgUrl;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public void setPrice(Double price) {
+            this.price = price;
+        }
+
+        public void setImgUrl(String imgUrl) {
+            this.imgUrl = imgUrl;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public Double getPrice() {
+            return price;
+        }
+
+        public String getImgUrl() {
+            return imgUrl;
+        }
+
+        public Set<Category> getCategories() {
+            return categories;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            com.teteuweb.project.entities.Product product = (com.teteuweb.project.entities.Product) o;
+            return Objects.equals(id, product.id);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(id);
+        }
     }
-
-    public Product(Long id, String name, String description, Double price, String imgUrl) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.imgUrl = imgUrl;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public Set<Category> getCategories() {
-        return categories;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return Objects.equals(id, product.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-}
