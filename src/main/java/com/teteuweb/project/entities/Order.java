70e00953bad03cf8e0761a98ba2f86340d4206aa
Zé Matheus <josemw86@gmail.com>
2024-09-05 19:30:47 -0300

    Payment, one-to-one association

--- src/main/java/com/teteuweb/project/entities/Order.java
+++ src/main/java/com/teteuweb/project/entities/Order.java
@@ -30,6 +30,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -82,6 +85,14 @@ public class Order implements Serializable {
             this.orderStatus = orderStatus.getCode();
         }
 
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
