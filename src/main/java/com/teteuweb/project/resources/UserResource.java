d093b75dc152ee2d017414aaf824627a86dfa257
Zé Matheus <josemw86@gmail.com>
2024-09-09 20:45:59 -0300

    User update

--- src/main/java/com/teteuweb/project/resources/UserResource.java
+++ src/main/java/com/teteuweb/project/resources/UserResource.java
@@ -42,4 +42,11 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
 }
--- src/main/java/com/teteuweb/project/services/UserService.java
+++ src/main/java/com/teteuweb/project/services/UserService.java
@@ -26,7 +26,21 @@ public class UserService {
     public User insert(User obj) {
         return repository.save(obj);
     }
+
     public void delete(Long id) {
      repository.deleteById(id);
     }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
 }
