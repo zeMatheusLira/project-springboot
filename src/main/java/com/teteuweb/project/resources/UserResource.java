22192b6565ff28a207a76b65c75667ffe11e9fbc
Zé Matheus <josemw86@gmail.com>
2024-09-09 20:33:56 -0300

    User delete

--- src/main/java/com/teteuweb/project/resources/UserResource.java
+++ src/main/java/com/teteuweb/project/resources/UserResource.java
@@ -36,4 +36,10 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/teteuweb/project/services/UserService.java
+++ src/main/java/com/teteuweb/project/services/UserService.java
@@ -26,4 +26,7 @@ public class UserService {
     public User insert(User obj) {
         return repository.save(obj);
     }
+    public void delete(Long id) {
+     repository.deleteById(id);
+    }
 }
