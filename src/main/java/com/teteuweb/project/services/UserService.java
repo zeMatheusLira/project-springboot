84d4ef4e38a3fe3733e2d93cf26a06491e717e64
Zé Matheus <josemw86@gmail.com>
2024-09-10 22:43:19 -0300

    Exception handling - update

--- src/main/java/com/teteuweb/project/services/UserService.java
+++ src/main/java/com/teteuweb/project/services/UserService.java
@@ -4,6 +4,7 @@ import com.teteuweb.project.entities.User;
 import com.teteuweb.project.repositories.UserRepository;
 import com.teteuweb.project.services.exceptions.DatabaseException;
 import com.teteuweb.project.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,15 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
